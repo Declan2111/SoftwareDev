88d07244f4150d37611e883df266169521f5077f
Ella Zaugg-James <ellaz-j@MacBook-Pro-4.local>
2024-03-19 21:33:46 +0100

    added function 1 and corresponding tests

--- src/main/python/UC3MTravel/HotelReservation.py
+++ src/main/python/UC3MTravel/HotelReservation.py
@@ -1,10 +1,12 @@
 
 import hashlib
 import json
+from stdnum import es
 from datetime import datetime
+import re
 
 class HotelReservation:
-    def __init__(self, IDCARD, creditcardNumb, nAMeAndSURNAME, phonenumber, room_type,numdays):
+    def __init__(self, IDCARD, creditcardNumb, nAMeAndSURNAME, phonenumber, room_type, numdays):
         self.__crEDITcardnumber = creditcardNumb
         self.__idcard = IDCARD
         justnow = datetime.utcnow()
@@ -17,6 +19,19 @@ class HotelReservation:
     def __str__(self):
         """return a json string with the elements required to calculate the localizer"""
         #VERY IMPORTANT: JSON KEYS CANNOT BE RENAMED
+        if not self.checkCardNum(self.__crEDITcardnumber):
+            raise ValueError("Invalid credit card number.")
+        if not self.checkID(self.__idcard):
+            raise ValueError("Invalid ID card.")
+        if not self.checkName(self.__NAME_SURNAME):
+            raise ValueError(
+                "Name and surname must be between 10 and 50 characters and contain at least two strings separated by white space.")
+        if not self.checkPhone(self.__phonenumber):
+            raise ValueError("Invalid phone number.")
+        if not self.checkRoom(self.__roomtype):
+            raise ValueError("Invalid room type.")
+        if not self.checkNumDays(self.__num_days):
+            raise ValueError("Number of days must be a number between 1 and 10.")
         json_info = {"id_card": self.__idcard,
                      "name_surname": self.__NAME_SURNAME,
                      "credit_card": self.__crEDITcardnumber,
@@ -40,8 +55,52 @@ class HotelReservation:
     def IDCARD(self, value):
         self.__idcard = value
 
-
     @property
     def LOCALIZER( self ):
         """Returns the md5 signature"""
         return hashlib.md5(self.__str__().encode()).hexdigest()
+
+    def checkCardNum(self, card_number):
+        card_str = str(card_number)
+        if len(str(card_number)) != 16:
+            return False
+
+        total = 0
+        for i in range(15):  # Loop through the first 15 digits
+            digit = int(card_str[i])
+            if i % 2 != 0:  # Only double the digit if its position is odd
+                doubled_digit = digit * 2
+                if doubled_digit > 9:
+                    doubled_digit -= 9
+                total += doubled_digit
+
+        return total % 10 == int(card_str[15]) and len(str(card_number)) == 16
+
+    def checkID(self, IDNum):
+        return es.dni.is_valid(IDNum)
+
+    def checkName(self, name):
+        if isinstance(name, str):
+            return 10 <= len(name) <= 50 and ' ' in name.strip()
+        else:
+            return False
+
+    def checkPhone(self, PhoneNum):
+        if isinstance(PhoneNum, int):
+            return len(str(PhoneNum)) == 9 and int(PhoneNum) > 0
+        else:
+            return False
+
+    def checkRoom(self, roomType):
+        if isinstance(roomType, str):
+            return roomType.lower() in ["single", "double", "suite"]
+        else:
+            return False
+
+    def checkNumDays(self, numDays):
+        if isinstance(numDays, int):
+            return 0 < numDays < 11
+        else:
+            return False
+
+
---
+++ src/unittest/python/TestHotelReservation.py
@@ -0,0 +1,161 @@
+import unittest
+
+from src.main.python.UC3MTravel.HotelReservation import HotelReservation
+
+ValidCardNum = 4929319438123457
+ValidID = "12345678Z"
+ValidName = "Ella Zaugg-James"
+ValidPhone = 123456789
+ValidRoom = "single"
+ValidNumDays = 5
+ValidClass = HotelReservation(ValidID, ValidCardNum, ValidName, ValidPhone, ValidRoom, ValidNumDays)
+
+
+class TestHotelReservation(unittest.TestCase):
+    # tests a valid card number
+    def testValidCard1(self):
+        self.assertTrue(ValidClass.checkCardNum(ValidCardNum))
+
+    # tests an invalid card number - not luhns compliant
+    def testInvalidCard1(self):
+        self.assertFalse(ValidClass.checkCardNum(4929319438123450))
+
+    # tests an invalid card number - incorrect data type
+    def testInvalidCard2(self):
+        self.assertFalse(ValidClass.checkCardNum("abcdefgh"))
+
+    # tests an invalid card number - too many digits
+    def testInvalidCard3(self):
+        self.assertFalse(ValidClass.checkCardNum(49293194381234578))
+
+    # tests an invalid card number - too few digits
+    def testInvalidCard4(self):
+        self.assertFalse(ValidClass.checkCardNum(492931943812345))
+
+    # tests a valid ID value
+    def testValidID1(self):
+        self.assertTrue(ValidClass.checkID(ValidID))
+
+    # tests an invalid ID value - letter does not correspond with number
+    def testInvalidID1(self):
+        self.assertFalse(ValidClass.checkID("12345678A"))
+
+    # tests an invalid ID value - too many characters
+    def testInvalidID2(self):
+        self.assertFalse(ValidClass.checkID("123456789A"))
+
+    # tests an invalid ID value - too few characters
+    def testInvalidID3(self):
+        self.assertFalse(ValidClass.checkID("1234567A"))
+
+    # tests an invalid ID value - letter not at the end
+    def testInvalidID4(self):
+        self.assertFalse(ValidClass.checkID("Z12345678"))
+
+    # tests an invalid ID data type
+    def testInvalidID5(self):
+        self.assertFalse(ValidClass.checkID(12345678.9))
+
+    # tests a valid name
+    def testValidName1(self):
+        self.assertTrue(ValidClass.checkName(ValidName))
+
+    # tests boundary value of 10 characters
+    def testValidName2(self):
+        self.assertTrue(ValidClass.checkName("John Smith"))
+
+    # tests boundary value of 11 characters
+    def testValidName3(self):
+        self.assertTrue(ValidClass.checkName("Jonny Smith"))
+
+    # tests boundary value of 49 characters
+    def testValidName4(self):
+        self.assertTrue(ValidClass.checkName("Alexandria Elizabeth Catherine Stephanie Jonathan"))
+
+    # tests boundary value of 50 characters
+    def testValidName5(self):
+        self.assertTrue(ValidClass.checkName("Alexandria Elizabeth Catherine Stephanie Jonathans"))
+
+    # tests boundary value of 51 characters
+    def testInvalidName1(self):
+        self.assertFalse(ValidClass.checkName("Alexandria Elizabeth Catherines Stephanie Jonathans"))
+
+    # tests boundary value of 9 characters
+    def testInvalidName2(self):
+        self.assertFalse(ValidClass.checkName("Jon Smith"))
+
+    # tests incorrect data type
+    def testInvalidName3(self):
+        self.assertFalse(ValidClass.checkName(1234))
+
+    # tests name with no space in string
+    def testInvalidName4(self):
+        self.assertFalse(ValidClass.checkName("DeclanLowney"))
+
+    # tests a valid phone number
+    def testValidPhone1(self):
+        self.assertTrue(ValidClass.checkPhone(ValidPhone))
+
+    # tests an invalid phone number boundary value
+    def testInvalidPhone1(self):
+        self.assertFalse(ValidClass.checkPhone(12345678))
+
+    # tests an invalid phone number boundary value
+    def testInvalidPhone2(self):
+        self.assertFalse(ValidClass.checkPhone(1234567891))
+
+    # tests an invalid phone number data type
+    def testInvalidPhone3(self):
+        self.assertFalse(ValidClass.checkPhone("hellooooo"))
+
+    # tests room of type single
+    def testValidRoom1(self):
+        self.assertTrue(ValidClass.checkRoom(ValidRoom))
+
+    # tests room of type double
+    def testValidRoom2(self):
+        self.assertTrue(ValidClass.checkRoom("double"))
+
+    # tests room of type suite
+    def testValidRoom3(self):
+        self.assertTrue(ValidClass.checkRoom("suite"))
+
+    # tests a string which is not a room
+    def testInvalidRoom1(self):
+        self.assertFalse(ValidClass.checkRoom("hello"))
+
+    # tests an incorrect data type
+    def testInvalidRoom2(self):
+        self.assertFalse(ValidClass.checkRoom(1234))
+
+    # tests a valid number of days to stay
+    def testValidNumDays1(self):
+        self.assertTrue(ValidClass.checkNumDays(ValidNumDays))
+
+    # tests boundary value of valid number of days to stay
+    def testValidNumDays2(self):
+        self.assertTrue(ValidClass.checkNumDays(1))
+
+    # tests boundary value of valid number of days to stay
+    def testValidNumDays3(self):
+        self.assertTrue(ValidClass.checkNumDays(2))
+
+    # tests boundary value of valid number of days to stay
+    def testValidNumDays3(self):
+        self.assertTrue(ValidClass.checkNumDays(9))
+
+    # tests boundary value of valid number of days to stay
+    def testValidNumDays4(self):
+        self.assertTrue(ValidClass.checkNumDays(10))
+
+    # tests boundary value of invalid number of days to stay
+    def testInvalidNumDays4(self):
+        self.assertFalse(ValidClass.checkNumDays(11))
+
+    # tests boundary value of invalid number of days to stay
+    def testInvalidNumDays5(self):
+        self.assertFalse(ValidClass.checkNumDays(0))
+
+    # tests an incorrect data type for num days
+    def testInvalidNumDays6(self):
+        self.assertFalse(ValidClass.checkNumDays("a"))
